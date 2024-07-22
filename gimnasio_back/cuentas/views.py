119a45a5f0c84a28c03ad92146ed84e751b1c181
liqui0-blvck <nicolasmacgwrk@gmail.com>
2024-07-22 13:04:34 -0400

    actualizacion perfil agregado

--- gimnasio_back/cuentas/views.py
+++ gimnasio_back/cuentas/views.py
@@ -2,12 +2,48 @@
 from rest_framework import  viewsets
 from cuentas.serializers import ActividadSerializer, ConfiguracionUsuarioSerializer, InteresesUsuarioSerializer, PerfilSerializer, RegistroActividadSerializer
 from .models import *
+from core.serializers import *
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 
 
     
 class PerfilViewSet(viewsets.ModelViewSet):
+    lookup_field = 'usuario__pk'
     queryset = Perfil.objects.all()
     serializer_class = PerfilSerializer
+    
+    def get_queryset(self):
+        # Retorna el perfil del usuario logueado
+        return Perfil.objects.filter(usuario=self.request.user)
+
+    @action(detail=False, methods=['patch'], url_path='actualizar_perfil')
+    def actualizar_perfil(self, request):
+        perfil = Perfil.objects.get(usuario=request.user)
+        perfil_data = request.data.get('perfil_data', {})
+        usuario_data = request.data.get('usuario_data', {})
+
+        # Actualizar perfil
+        perfil_serializer = PerfilSerializer(perfil, data=perfil_data, partial=True)
+        if perfil_serializer.is_valid():
+            perfil_serializer.save()
+        else:
+            return Response(perfil_serializer.errors)
+
+        # Actualizar usuario
+        usuario_serializer = CustomUserSerializer(request.user, data=usuario_data, partial=True)
+        if usuario_serializer.is_valid():
+            usuario_serializer.save()
+        else:
+            return Response(usuario_serializer.errors)
+
+        return Response({
+            "perfil": perfil_serializer.data
+        })
+
+        # Si necesitas actualizar campos específicos del perfil
+
     
 class ConfiguracionUsuarioViewSet(viewsets.ModelViewSet):
     queryset = ConfiguracionUsuario.objects.all()
