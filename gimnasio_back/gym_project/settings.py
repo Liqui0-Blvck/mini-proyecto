60aac87bf43f0fd004be660374ef4bb038938ad8
Liqui0-Blvck <nicolasmacgwrk@gmail.com>
2024-07-27 09:56:10 -0400

    algo

--- gimnasio_back/gym_project/settings.py
+++ gimnasio_back/gym_project/settings.py
@@ -66,8 +66,8 @@ DJOSER = {
     },
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
