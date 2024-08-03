ae0f83faa5fa4c58aa00b47e7aaf2f0735cca190
liqui0-blvck <nicolasmacgwrk@gmail.com>
2024-08-03 15:11:08 -0400

    cambios en acceso al gimnasio

--- gimnasio_back/gimnasios/views.py
+++ gimnasio_back/gimnasios/views.py
@@ -1,10 +1,21 @@
 from rest_framework import viewsets
 from .models import *
 from .serializers import *
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
 
 class GimnasioViewSet(viewsets.ModelViewSet):
     queryset = Gimnasio.objects.all()
     serializer_class = GimnasioSerializer
+    
+    @action(detail=False, methods=['get'], url_path='gimnasio')
+    def obtener_gimnasio_dueno(self, request):
+        dueno = request.query_params.get('dueno')
+        gimnasio = Gimnasio.objects.get(dueno=dueno)
+        serializer = self.get_serializer(gimnasio)
+        return Response(serializer.data)
 
 class SucursalViewSet(viewsets.ModelViewSet):
     queryset = Sucursal.objects.all()
